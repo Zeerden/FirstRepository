4625dd16eb70a938454e7556684be1981478a417
Zeerden <zeyupogluerden@gmail.com>
2022-10-12 14:04:26 -0700

    first commit

---
+++ ZMain.java
@@ -0,0 +1,14 @@
+package zeynep.main;
+
+public class ZMain {
+
+	public ZMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+	System.out.println("welcome on board!");
+
+	}
+
+}
---
+++ ZeynepStudy.java
@@ -0,0 +1,10 @@
+package zeynep.com;
+
+public class ZeynepStudy {
+
+	public static void main(String[] args) {
+		System.out.println("If you study every day, you will reach your goal sooner!");
+
+	}
+
+}
